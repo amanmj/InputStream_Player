005d4d2a3589a19f37514a7cad9b874e3e667ef6
Aman Mahajan <amanmj95@gmail.com>
2016-01-19 20:25:27 +0530

    second commit

--- app/src/main/java/com/example/amanmj/inputstream_player/MainActivity.java
+++ app/src/main/java/com/example/amanmj/inputstream_player/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.amanmj.inputstream_player;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,31 +11,93 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 
 import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.FrameworkSampleSource;
+import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.SampleSource;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private ExoPlayer exoPlayer;
     private int rendererCount;
+    File file;
+    SeekBar seekBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         rendererCount=1;
         exoPlayer= ExoPlayer.Factory.newInstance(rendererCount);
+        /* location of file in the root directory of SD Card named "song.mp3" */
+        file=new File(Environment.getExternalStorageDirectory(),"song.mp3");
+        /*check if file is present or not*/
+        try
+        {
+            if(file.exists()==false)
+            {
+                throw new FileNotFoundException("File not present in root of SD Card");
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
 
 
 
+
+
+
+
+
+
+
+        /*
+        seekbar Listener to seek exoplayer to different duration
+         */
+        seekBar=(SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    long newPos = (progress * exoPlayer.getDuration()) / 100;
+                    exoPlayer.seekTo(newPos);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
-    @Override
+    /*@Override
     public long getAvailableBytes() {
         if(counter.get() > file.length())
             return file.length();
         else
             return counter.get();
-    }
+    }*/
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
 }
